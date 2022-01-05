1b4ac93d0d254ade2ead55fe4e65cacab1e073ca
Davi <davifrancisco214@outlook.com>
2022-01-04 22:10:37 -0300

    Evitando que a aplicação pare de funcionar ao requisitar um recurso que não existe.

--- ByteBankApp/Infrastructure/WebApplication.cs
+++ ByteBankApp/Infrastructure/WebApplication.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Net;
+using System.Reflection;
 using System.Text;
+using ByteBankApp.Utilities;
 
 namespace ByteBankApp.Infrastructure
 {
@@ -14,6 +16,14 @@ namespace ByteBankApp.Infrastructure
         }
         
         public void Initialize()
+        {
+            while (true)
+            {
+               HandleRequests();
+            }
+        }
+
+        private void HandleRequests()
         {
             // Objeto responsável por responder as requisições HTTP.
             HttpListener httpListener = new();
@@ -32,19 +42,33 @@ namespace ByteBankApp.Infrastructure
             HttpListenerContext context = httpListener.GetContext();
             var request = context.Request;
             var response = context.Response;
-            
+
             // Para adicionar um conteúdo ao response é necessário converter 
             // o conteúdo em um stream.
-            var content = "Olá, mundo!";
-            var contentStream = Encoding.UTF8.GetBytes(content);
+            // Obtendo o caminho do recurso dentro do assembly.
+            var resourcePath = Util.GetResourceFromAssembly(request.Url.AbsolutePath);
+            var resource = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourcePath);
             
-            response.ContentType = "text/html; charset=utf-8";
-            response.StatusCode = 200;
-            // Define o tamonho da resposta.
-            response.ContentLength64 = contentStream.Length;
-            // Escrevendo a resposta no body.
-            response.OutputStream.Write(contentStream, 0, contentStream.Length);
-            response.OutputStream.Close();
+            if (resource == null)
+            {
+                response.StatusCode = 404;
+                response.OutputStream.Close();
+            }
+            else
+            {
+                // Acessando o recurso dentro do assembly.
+                byte[] buffer = new byte[resource.Length];
+            
+                // Adicionando os dados do stream no buffer.
+                resource.Read(buffer, 0, (int) resource.Length);
+                response.ContentType = Util.GetContentType(resourcePath);
+                response.StatusCode = 200;
+                // Define o tamonho da resposta.
+                response.ContentLength64 = resource.Length;
+                // Escrevendo a resposta no body.
+                response.OutputStream.Write(buffer, 0, buffer.Length);
+                response.OutputStream.Close();
+            }
             
             httpListener.Stop();
         }
