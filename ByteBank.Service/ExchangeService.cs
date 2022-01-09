ef6ce7dcb8e7c115d5c62e185b6494eb6df6e3d1
Davi <davifrancisco214@outlook.com>
2022-01-09 13:29:16 -0300

    Criando o serviço que calcula a cotação

---
+++ ByteBank.Service/ExchangeService.cs
@@ -0,0 +1,15 @@
+﻿using ByteBank.Service.Interfaces;
+using System;
+
+namespace ByteBank.Service
+{
+    public class ExchangeService : IExchangeService
+    {
+        private readonly Random _random = new Random();
+
+        public decimal Calculate(string originCurrency, string targetCurrency, decimal amount)
+        {
+            return (decimal) _random.NextDouble() * amount;
+        }
+    }
+}
---
+++ ByteBank.Service/Interfaces/IExchangeService.cs
@@ -0,0 +1,7 @@
+﻿namespace ByteBank.Service.Interfaces
+{
+    public interface IExchangeService
+    {
+        decimal Calculate(string originCurrency, string targetCurrency, decimal amount);
+    }
+}
