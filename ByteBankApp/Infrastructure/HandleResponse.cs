4f989eb520b0eb2bb06293d018f4c8da7b898fc3
Davi <davifrancisco214@outlook.com>
2022-01-09 21:29:42 -0300

    Criando uma class responsavel por responder as requisições

---
+++ ByteBankApp/Infrastructure/HandleResponse.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Reflection;
+
+namespace ByteBankApp.Infrastructure
+{
+    public class HandleResponse
+    {
+        public void Handle(HttpListenerResponse response, string path)
+        {
+            // Para adicionar um conteúdo ao response é necessário converter 
+            // o conteúdo em um stream.
+            var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
+
+            if (resourceStream == null)
+            {
+                response.StatusCode = 404;
+                response.OutputStream.Close();
+            }
+            else
+            {
+                byte[] buffer = new byte[resourceStream.Length];
+                resourceStream.Read(buffer, 0, (int)resourceStream.Length);
+                response.StatusCode = 200;
+                response.ContentLength64 = resourceStream.Length;
+                response.ContentType = Utilities.Util.GetContentType(path);
+                response.OutputStream.Write(buffer, 0, buffer.Length);
+                response.OutputStream.Close();
+                resourceStream.Close();
+            }
+        }
+    }
+}
--- ByteBankApp/Infrastructure/WebApplication.cs
+++ ByteBankApp/Infrastructure/WebApplication.cs
@@ -47,33 +47,12 @@ namespace ByteBankApp.Infrastructure
 
             if (Util.IsAFile(request.Url.AbsolutePath))
             {
-                // Para adicionar um conteúdo ao response é necessário converter 
-                // o conteúdo em um stream.
                 // Obtendo o caminho do recurso dentro do assembly.
                 var resourcePath = Util.GetResourceFromAssembly(request.Url.AbsolutePath);
-                var resource = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourcePath);
-
-                if (resource == null)
-                {
-                    response.StatusCode = 404;
-                    response.OutputStream.Close();
-                }
-                else
-                {
-                    byte[] buffer = new byte[resource.Length];
-
-                    // Adicionando os dados do stream no buffer.
-                    resource.Read(buffer, 0, (int)resource.Length);
-                    response.ContentType = Util.GetContentType(resourcePath);
-                    response.StatusCode = 200;
-                    // Define o tamonho da resposta.
-                    response.ContentLength64 = resource.Length;
-                    // Escrevendo a resposta no body.
-                    response.OutputStream.Write(buffer, 0, buffer.Length);
-                    response.OutputStream.Close();
-                }
+                HandleResponse handleResponse = new HandleResponse();
+                handleResponse.Handle(response, resourcePath);
             }
-            else if (request.Url.AbsolutePath == "/Cambio/MXN")
+            else if (request.Url.AbsolutePath == "/Exchange/MXN")
             {
                 ExchangeController exchangeController = new ExchangeController();
                 var content = exchangeController.GetMXN();
