5a2cdcfec0617d11753ba9136d43d9e67c92666f
Davi <davifrancisco214@outlook.com>
2022-01-09 22:46:58 -0300

    Criando uma classe para retornar o conteudo de uma página

---
+++ ByteBankApp/Controllers/ControllerBase.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace ByteBankApp.Controllers
+{
+    public abstract class ControllerBase
+    {
+        // CallerMemberName retorna o nome do método que chamou este método.
+        protected string View([CallerMemberName] string actionName = null)
+        {
+            string content = null;
+
+            // Pega o nome da classe filha.
+            string controller = GetType().Name.Replace("Controller", "");
+
+            string resourcePath = $"ByteBankApp.View.{controller}.{actionName}.html";
+            Stream resourceStream = Assembly.GetExecutingAssembly()
+                .GetManifestResourceStream(resourcePath);
+
+            using (StreamReader reader = new(resourceStream))
+            {
+                content = reader.ReadToEnd();
+            }
+
+            return content;
+        }
+    }
+}
--- ByteBankApp/Controllers/ExchangeController.cs
+++ ByteBankApp/Controllers/ExchangeController.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 
 namespace ByteBankApp.Controllers
 {
-    public class ExchangeController
+    public class ExchangeController : ControllerBase
     {
         private readonly IExchangeService _exchangeService;
 
@@ -16,34 +16,17 @@ namespace ByteBankApp.Controllers
 
         public string MXN()
         {
-            string html = null;
-
+            string html = View();
             decimal value = _exchangeService.Calculate("MXN", "BRL", 1);
-            var resource = "ByteBankApp.View.Exchange.MXN.html";
-            var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resource);
-
-            // Convertendo o resourceStream em uma cadeia de caracteres.
-            using (StreamReader reader = new(resourceStream))
-            {
-                html = reader.ReadToEnd();
-            }
-
+            
             return html.Replace("$VALOR_MAGICO", value.ToString("c2"));
         }
 
         public string USD()
         {
-            string html = null;
-
+            string html = View();
             decimal value = _exchangeService.Calculate("USD", "BRL", 1);
-            var resource = "ByteBankApp.View.Exchange.USD.html";
-            var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resource);
-
-            using (StreamReader reader = new(resourceStream))
-            {
-                html = reader.ReadToEnd();
-            }
-
+            
             return html.Replace("$VALOR_MAGICO", value.ToString("c2"));
         }
     }
